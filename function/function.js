e5cc222643945c222ef38fce4574757cab5bd929
devRokib <rakibahmedra971162@gmail.com>
2024-02-18 06:08:43 +0000

    test

--- function/function.js
+++ function/function.js
@@ -13,3 +13,30 @@ function sayMyName (){
     console.log("hello bangladesh")
 }
 sayMyName()
+
+
+// function sometest(numberOne,numberTwo){
+//     return(numberOne + numberTwo)
+// }
+
+// const result = sometest(12,10)
+// console.log(result)
+
+// function sometest(numberOne,numberTwo){
+//  const result = numberOne + numberTwo
+//  return result
+// }
+
+//  const newResult = sometest(12,10)
+//  console.log(newResult)
+
+
+function sometest(numberOne,numberTwo){
+ const result = numberOne + numberTwo
+ return result
+ console.log("hello world") // not worked cause after reutrn any code are not worked
+}
+
+ const newResult = sometest(12,10)
+ console.log(newResult)
+// console.log(result)
